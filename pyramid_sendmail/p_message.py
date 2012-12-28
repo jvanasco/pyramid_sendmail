ca387e8c54e4ed2cb18eafa59b021bf63d3e093c
Parker Pinette <parker@parkerpinette.com>
2012-12-27 20:51:15 -0800

    pep8 compliance and cleanup

--- pyramid_sendmail/p_message.py
+++ pyramid_sendmail/p_message.py
@@ -1,8 +1,10 @@
 from pyramid_mailer.message import Attachment as PmAttachment
 from pyramid_mailer.message import Message as PmMessage
 
+
 class Attachment(PmAttachment):
     pass
+
 
 class Message(PmMessage):
     pass
